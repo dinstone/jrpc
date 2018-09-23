09e28e3264ca4fb3d61e6222762112ca34aefc6f
dinstone <dinstone@163.com>
2018-09-23 20:43:50 +0800

    edit Copyright

--- jrpc-transport-mina/src/main/java/com/dinstone/jrpc/transport/mina/TransportProtocolEncoder.java
+++ jrpc-transport-mina/src/main/java/com/dinstone/jrpc/transport/mina/TransportProtocolEncoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014~2017 dinstone<dinstone@163.com>
+ * Copyright (C) 2013~2017 dinstone<dinstone@163.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
